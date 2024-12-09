5c6b625e68dfc8c83826f1f9c576c3eeed036d21
Gary <hai.alphacreative@gmail.com>
2024-12-09 11:13:08 +0700

    a

--- horizontal-slider-image/script.js
+++ horizontal-slider-image/script.js
@@ -1,10 +1,49 @@
-let target = 0;
-let current = 0;
-let ease = 0.75;
+// Lấy tất cả các phần tử wrapper-testimonial
+const wrapperTestimonials = document.querySelectorAll(".slider-wrapper");
 
-const slider = document.querySelector(".slider");
-const sliderWrapper = document.querySelector(".slider-wrapper");
-const markerWrapper = document.querySelector(".marker-wrapper");
-const activeSlide = document.querySelector(".active-slide");
+// Nếu có bất kỳ phần tử testimonial nào
+if (wrapperTestimonials.length > 0) {
+  // Lặp qua từng phần tử wrapper
+  wrapperTestimonials.forEach((wrapper) => {
+    const testimonial = wrapper.querySelector(".slider");
 
-let maxScroll = sliderWrapper.offsetWidth - window.innerWidth;
+    // Hàm để tính toán lượng cuộn cần thiết
+    const getScrollAmount = () => {
+      const racesWidth = testimonial.scrollWidth;
+      return racesWidth - window.innerWidth + 200;
+    };
+
+    // Hàm để tạo tween animation cho testimonial
+    const createTween = (testimonial, scrollAmount) => {
+      return gsap.to(testimonial, {
+        x: -scrollAmount,
+        duration: 3,
+        ease: "none",
+      });
+    };
+
+    // Hàm để tạo ScrollTrigger cho phần tử wrapper
+    const createScrollTrigger = (wrapper, tween, scrollAmount) => {
+      ScrollTrigger.create({
+        trigger: wrapper,
+        start: "top 15%",
+        end: `+=${scrollAmount}`,
+        pin: true,
+        animation: tween,
+        scrub: 1,
+        invalidateOnRefresh: true,
+        markers: true,
+      });
+    };
+
+    // Tính toán lượng cuộn cần thiết
+    const scrollAmount = getScrollAmount();
+    // Tạo tween animation cho testimonial
+    const tween = createTween(testimonial, scrollAmount);
+    // Tạo ScrollTrigger cho phần tử wrapper
+    createScrollTrigger(wrapper, tween, scrollAmount);
+  });
+
+  // Làm mới ScrollTrigger sau khi tất cả triggers đã được thiết lập
+  ScrollTrigger.refresh();
+}
