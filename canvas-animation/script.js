72ec34e8c19718c7646b88ffb2ae30bb7fa7a2e4
Gary <hai.alphacreative@gmail.com>
2024-12-30 14:47:54 +0700

    a

--- canvas-animation/script.js
+++ canvas-animation/script.js
@@ -16,10 +16,92 @@ function setup() {
   addBoundaries();
 
   for (let i = 0; i < 12; i++) {
-    let x = random(100, width - 100);
-    let y = random(100, height - 100);
+    let x = random(50, width - 100);
+    let y = random(50, height - 100);
     items.push(new Item(x, y, `./images/img${i + 1}.webp`));
   }
 }
 
-function addBoundaries() {}
+function addBoundaries() {
+  const thickness = 50;
+  World.add(engine.world, [
+    Bodies.rectangle(width / 2, -thickness / 2, width, thickness, {
+      isStatic: true,
+    }),
+    Bodies.rectangle(width / 2, height + thickness / 2, width, thickness, {
+      isStatic: true,
+    }),
+    Bodies.rectangle(-thickness / 2, height / 2, thickness, height, {
+      isStatic: true,
+    }),
+    Bodies.rectangle(width + thickness / 2, height / 2, thickness, height, {
+      isStatic: true,
+    }),
+  ]);
+}
+
+function draw() {
+  background("black");
+  Engine.update(engine); // Cập nhật trạng thái vật lý
+  items.forEach((item) => item.update());
+}
+
+class Item {
+  constructor(x, y, imagePath) {
+    let options = {
+      frictionAir: 0.075,
+      restitution: 0.25,
+      density: 0.002,
+      angle: Math.random() + Math.PI * 2,
+    };
+    this.body = Bodies.rectangle(x, y, 100, 200, options);
+
+    World.add(engine.world, this.body);
+
+    this.div = document.createElement("div");
+    this.div.className = "item";
+    this.div.style.left = `${this.body.position.x - 50}px`;
+    this.div.style.top = `${this.body.position.y - 100}px`;
+    const img = document.createElement("img");
+    img.src = imagePath;
+    this.div.appendChild(img);
+    document.body.appendChild(this.div);
+  }
+
+  update() {
+    this.div.style.left = `${this.body.position.x - 50}px`;
+    this.div.style.top = `${this.body.position.y - 100}px`;
+    this.div.style.transform = `rotate(${this.body.angle}rad)`;
+  }
+}
+
+function mouseMoved() {
+  if (dist(mouseX, mouseY, lastMouseX, lastMouseY) > 10) {
+    lastMouseX = mouseX;
+    lastMouseY = mouseY;
+
+    items.forEach((item) => {
+      console.log(
+        dist(mouseX, mouseY, item.body.position.x, item.body.position.y)
+      );
+
+      if (
+        dist(mouseX, mouseY, item.body.position.x, item.body.position.y) < 500
+      ) {
+        let forceMagnitude = 2;
+        console.log("aa");
+        Body.applyForce(
+          item.body,
+          {
+            x: item.body.position.x,
+            y: item.body.position.y,
+          },
+          {
+            x: random(-forceMagnitude, forceMagnitude),
+            y: random(-forceMagnitude, forceMagnitude),
+          }
+        );
+      }
+    });
+  }
+}
