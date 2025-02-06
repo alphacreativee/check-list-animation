2747ce6cd313522f7208c77e22f157a6d42be892
Gary <hai.alphacreative@gmail.com>
2025-02-06 12:08:24 +0700

    a

--- image-hover-distortion/script.js
+++ image-hover-distortion/script.js
@@ -1,9 +1,8 @@
 document.querySelectorAll(".wrapper-img").forEach((wrapper) => {
   new hoverEffect({
     parent: wrapper,
-    intensity1: 0.5,
-    intensity2: 0.4,
-    angle2: Math.PI / 2,
+    intensity: wrapper.getAttribute("data-intensity"),
+    angle: Math.PI / 2,
     image1: wrapper.getAttribute("data-image1"),
     image2: wrapper.getAttribute("data-image2"),
     displacementImage: wrapper.getAttribute("data-displacement"),
