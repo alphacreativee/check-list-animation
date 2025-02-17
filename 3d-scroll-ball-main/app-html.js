2a97b4d9ad0e615c0b7f2ea9769fb13cdc41f2c1
Gary <hai.alphacreative@gmail.com>
2025-02-17 10:52:26 +0700

    a

--- 3d-scroll-ball-main/app-html.js
+++ 3d-scroll-ball-main/app-html.js
@@ -1,3 +1,19 @@
+lenis = new Lenis({
+  lerp: 0.2, // Lower values create a smoother scroll effect
+  smoothWheel: true, // Enables smooth scrolling for mouse wheel events
+});
+
+// Update ScrollTrigger each time the user scrolls
+lenis.on("scroll", () => ScrollTrigger.update());
+
+// Define a function to run at each animation frame
+const scrollFn = (time) => {
+  lenis.raf(time); // Run Lenis' requestAnimationFrame method
+  requestAnimationFrame(scrollFn); // Recursively call scrollFn on each frame
+};
+// Start the animation frame loop
+requestAnimationFrame(scrollFn);
+
 const canvas = document.querySelector(".canvas");
 const context = canvas.getContext("2d");
 
